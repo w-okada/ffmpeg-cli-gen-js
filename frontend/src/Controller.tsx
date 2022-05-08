44b280be9f0b8e36071dfd8e0b94c29e6041a2ba
Wataru Okada <wataru.okada@flect.co.jp>
2022-05-08 10:05:08 +0900

    update

--- frontend/src/Controller.tsx
+++ frontend/src/Controller.tsx
@@ -10,6 +10,7 @@ export const Controller = () => {
         currentValue: inputSourceType || "File",
         onInputSourceTypeChanged: setInputSourceType,
         onInputSourceChanged: setInputSource,
+        onlyFile: true,
     };
 
     return (
