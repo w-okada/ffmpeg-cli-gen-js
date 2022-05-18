df190e551427e78b74d0795c6623c0c97620c726
Wataru Okada <wataru.okada@flect.co.jp>
2022-05-18 19:31:22 +0900

    update

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -5,7 +5,7 @@ export const Card = () => {
     return (
         <div className="m-4 rounded-xl bg-base-100 shadow-lg">
             <div className="m-4">
-                <h1 className="text-xl ">Super Simple movie editor</h1>
+                <h1 className="text-xl ">Super Simple movie editor with ffmpeg-wasm</h1>
                 <h2 className="text-base underline font-black pt-4">Usage</h2>
                 <p className="m-1">(1) push load file button and select movie file</p>
                 <p className="m-1">(2) select crop area from movie. set start time and end time by using time slide bar.</p>
@@ -20,29 +20,12 @@ export const Card = () => {
                 </p>
 
                 <h2 className="text-base underline font-black pt-4">link</h2>
-                <h2 className="text-base ">Quick Capture Screen</h2>
-                <p className="pl-4"> Capture your screen or window with just only browser</p>
-            </div>
-        </div>
-    );
-    return (
-        <div className="card">
-            <input type="radio" id="page1"></input>
-            <label htmlFor="page1">
-                <ul>
-                    <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
-                </ul>
-            </label>
-            <h1>SSME:Super Simple movie editor</h1>
-            <div id="desc-page1">
-                <h2>step1</h2>
-                <p></p>
-            </div>
-            <div id="desc-page2">
-                <h2>step2</h2>
-                <p></p>
+                <h2 className="text-base underline">
+                    <a className="text-base underline" href="https://screen-recorder-ts.herokuapp.com/frontend/index.html">
+                        Screen Capture with ffmpeg-wasm
+                    </a>
+                </h2>
+                <p className="pl-4">Capture your screen or window with just only browser </p>
             </div>
         </div>
     );
