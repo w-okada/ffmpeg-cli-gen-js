75d8b314f3df38725260f452bbff7d6494cd1969
Wataru Okada <wataru.okada@flect.co.jp>
2022-11-13 05:20:04 +0900

    update

--- frontend/src/002_hooks/000_useApplicationSettingManager.ts
+++ frontend/src/002_hooks/000_useApplicationSettingManager.ts
@@ -5,14 +5,16 @@ export type ApplicationSettingManagerStateAndMethod = {
     applicationSetting: ApplicationSetting | null
 }
 
+const LOCAL_STORAGE_PREFIX = location.pathname
+const LOCAL_STORAGE_APPLICATION_SETTING = `${LOCAL_STORAGE_PREFIX}_applicationSetting`
 export const useApplicationSettingManager = (): ApplicationSettingManagerStateAndMethod => {
     const [applicationSetting, setApplicationSetting] = useState<ApplicationSetting | null>(null)
 
     /** (1) Initialize Setting */
     /** (1-1) Load from localstorage */
     const loadApplicationSetting = async () => {
-        if (localStorage.applicationSetting) {
-            const applicationSetting = JSON.parse(localStorage.applicationSetting) as ApplicationSetting
+        if (localStorage[LOCAL_STORAGE_APPLICATION_SETTING]) {
+            const applicationSetting = JSON.parse(localStorage[LOCAL_STORAGE_APPLICATION_SETTING]) as ApplicationSetting
             console.log("Load AppStteing from Local Storage", applicationSetting)
             setApplicationSetting({ ...applicationSetting })
         } else {
